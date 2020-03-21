07e8e55b3cf272a85aab42bd0fc37d9a41754fab
kisa-josue <kisa.josue@lyceeppr.fr>
2020-03-21 15:31:57 +0100

    arrive pas a envoyer la requete sur adminer

--- main.cpp
+++ main.cpp
@@ -1,102 +1,67 @@
+//
+//  main.cpp
+//  snir1String
+//
+//  Created by Cécile Tonnerre on 11/03/2020.
+//  Copyright © 2020 Cécile Tonnerre. All rights reserved.
+//
+
+#include <iostream>
+// include pour les objets de type string C++
+#include <string>
+
+// include pour utiliser les string comme en C
 #include <string.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
-#include <fcntl.h> 		// descripteurs de fichiers ( open()...)
-#include <termios.h>	// 
+#include <fcntl.h> 
+#include <termios.h> 
 #include <errno.h>
-#include <iostream>
+#include <mariadb/mysql.h>
+using namespace std;
 
-/* baudrate settings are defined in <asm/termbits.h>, which is included by <termios.h> */
-#define BAUDRATE B115200            
-/* change this definition for the correct port */
-#define MODEMDEVICE "/dev/ttyS2"
-#define _POSIX_SOURCE 1 /* POSIX compliant source */
+#define PORTSERIE "/dev/ttyS2"
+#define BAUDRATE 115200
 
-#define FALSE 0
-#define TRUE 1
+int main(int argc, const char * argv[]) {
+    
+    /**************************************************************************/
+    /* utilisation des chaînes de caractères mode c++ : objet de type string  */
+    /**************************************************************************/
+    cout << "+++++++++++++++++++++++++++++++++++++++++++++++++++++"  << endl;
+    cout << "\tUtilisation en C++ de l'objet de type string " << endl; 
+    
+    // utilisation pour le TP
+    int sfd ;
+    string requete("INSERT INTO mesures  (temperature) VALUES ()");
+    char buf[255] = "Temp[0]: +19.31 degC";
+    string maTemperature ("NULL");
+    string buffer(buf);
+    string tempOutput;
+     char requeteFinale[1024]; //Sera utilisée pour la requete finale
+    cout << "ma ligne : " << buffer << endl;
+    
 
-volatile int STOP=FALSE; 
-int main()
-{
-  	int sfd, c, res;
-    struct termios newtio;
-    char buf[255];
-    ssize_t size;
-	
-	sfd = open(MODEMDEVICE, O_RDONLY | O_NOCTTY ); 
-	if (sfd == -1)
-	{
-	  printf ("Error  no is : %d\n", errno);
-	  printf("Error description is : %s\n",strerror(errno));
-	  return(-1);
-	}
-    bzero(&newtio, sizeof(newtio)); /* clear struct for new port settings */
- /* 
-          BAUDRATE: Set bps rate. You could also use cfsetispeed and cfsetospeed.
-          CRTSCTS : output hardware flow control (only used if the cable has
-                    all necessary lines. See sect. 7 of Serial-HOWTO)
-          CS8     : 8n1 (8bit,no parity,1 stopbit)
-          CLOCAL  : local connection, no modem contol
-          CREAD   : enable receiving characters
-        */
-    newtio.c_cflag = BAUDRATE | CRTSCTS | CS8 | CLOCAL | CREAD;
-         
-        /*
-          IGNPAR  : ignore bytes with parity errors
-          ICRNL   : map CR to NL (otherwise a CR input on the other computer
-                    will not terminate input)
-          otherwise make device raw (no other input processing)
-        */
-    newtio.c_iflag = IGNPAR | ICRNL;
-         
-       /*
-          ICANON  : enable canonical input
-          disable all echo functionality, and don't send signals to calling program
-        */
-    newtio.c_lflag = ICANON;
-         
 
-	/* 
-	  now clean the modem line and activate the settings for the port
-	*/
-	 tcflush(sfd, TCIFLUSH);
-	 tcsetattr(sfd,TCSANOW,&newtio);
-	
-	for (int i =0 ; i<50 ; i++)
-	{
-		
-	/*
-	  ** On lit :
-	  ** on passe a read :
-	  ** - le fd,
-	  ** - le buffer
-	  ** - la taille du buffer
-	  ** Attention si tu passe une taille de buffer plus grande que la taille de ton buffer,
-	  ** ton programme deviens sensible aux Buffer Overflow
-	  */
-	  size = read (sfd, buf, 127);
-   
-	  /*
-	  ** On raoute un '\0' à la fin de la chaine lut, pour être sur d'avoir une chaine de caractères valide.
-	  ** size correspondant a l'index du dernier caractere du buffer + 1.
-	  ** Ceci est utile si tu veux utiliser ta chaine dans une fonction comme strcmp() ou printf()
-	  */
- 
-	  buf[size] = 0;
-   
-	  /*
-	  ** On affiche ce que l'on viens de lire dans la console :
-	  ** NOTE :
-	  ** il existe des FD speciaux :
-	  ** Le fd 1 est la sortie standart ( console )
-	  */
- 
-	  //write (1, buf, size);
-	  std::cout << i << " " << buf << "\n";
-	  }
- 
-  /* Ne pas oublier de libérer ton file descriptor */
-  close(sfd);
- 
-  return 0;
+   if ( buffer.find("Temp[0]") != string::npos  )
+    {
+    	cout << "C'est gagné on a la temperature   " << endl ;
+    	buffer.replace(0, 9, "");
+    	cout << "ma ligne apres replace 1: -" << buffer << "- " << endl;
+    	buffer.replace(6, 9, "");
+    	cout << "ma ligne apres replace 2: -" << buffer << "- " << endl;
+
+    }
+    
+    
+    //decoupe (tokenzie) avec 
+    return 0;
+
+
+
+ close(sfd);//Ferme la lecture du port série
 }
+   
+
--- mysql.cpp
+++ mysql.cpp
@@ -1,25 +1,70 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <mariadb/mysql.h>
-int main(void)
+#include <string.h> //Gestion de string
+#include <unistd.h>//Pour interagir avec le port série
+#include <fcntl.h>//Descripteurs de fichiers
+#include <termios.h>//Contrôle des ports de communication asynchrone
+#include <errno.h>//Gestion des erreurs
+#include <mariadb/mysql.h>//Pour utiliser mysql
+#include <iostream>//Permet l'utilisation du cout
+using namespace std;//Pour ne pas avoir à rajouter des std::
+  int main(void)
 {
+  
+
+
+
+
+ // utilisation pour le TP
+    int sfd ;
+    string requete("INSERT INTO mesures  (temperature) VALUES ()");
+    char buf[255] = "Temp[0]: +19.31 degC";
+    string maTemperature ("NULL");
+    string buffer(buf);
+    string tempOutput;
+     char requeteFinale[1024]; //Sera utilisée pour la requete finale
+    cout << "ma ligne : " << buffer << endl;
+    
+
+
+   if ( buffer.find("Temp[0]") != string::npos  )
+    {
+    	cout << "C'est gagné on a la temperature   " << endl ;
+    	buffer.replace(0, 9, "");
+    	cout << "ma ligne apres replace 1: -" << buffer << "- " << endl;
+    	buffer.replace(6, 9, "");
+    	cout << "ma ligne apres replace 2: -" << buffer << "- " << endl;
+
+    }
+    
+    
+    //decoupe (tokenzie) avec 
+    return 0;
+
+
+
+
+
+
 MYSQL *conn;
-if ((conn = mysql_init(NULL)) == NULL)
+  if ((conn = mysql_init(NULL)) == NULL)
 {
-fprintf(stderr, "Could not init DB\n");
-return EXIT_FAILURE;
+  fprintf(stderr, "Could not init DB\n");
+  return EXIT_FAILURE;
 }
-if (mysql_real_connect(conn, "localhost", "bts", "snir", "capteurs", 0, NULL,
-0) == NULL)
+  if (mysql_real_connect(conn, "localhost", "bts", "snir", "capteurs", 0, NULL,
+  0) == NULL)
 {
-fprintf(stderr, "DB Connection Error\n");
-return EXIT_FAILURE;
+  fprintf(stderr, "DB Connection Error\n");
+  return EXIT_FAILURE;
 }
-if (mysql_query(conn, "INSERT INTO mesures (temperature) VALUES ('20')") != 0)
+
+  if (mysql_query(conn, "INSERT INTO mesures (temperature) VALUES ('18')") != 0)
 {
-fprintf(stderr, "Query Failure\n");
-return EXIT_FAILURE;
+  fprintf(stderr, "Query Failure\n");
+  return EXIT_FAILURE;
 }
-mysql_close(conn);
-return EXIT_SUCCESS;
+
+  mysql_close(conn);
+  return EXIT_SUCCESS;
 }
